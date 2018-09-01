e281914f2fa57c362772a153f38e30cfb589bab7
defneikiz <dikiz1@babson.edu>
2018-08-31 23:13:24 -0400

    Excercise 2 completed

---
+++ session01/calc.py
@@ -0,0 +1,13 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ((42*60)+42)
+2562
+>>> print (10*1.61)
+16.1
+>>> print(10/1.61)
+6.211180124223602
+>>> print(2562/6.21)
+412.56038647342996
+>>> print (412.56038647342996/60)
+6.876006441223833
+>>> 
